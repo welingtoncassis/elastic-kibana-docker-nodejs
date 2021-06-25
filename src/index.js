72e64e122fd4a3787763380559b1f34f5dfaf2a0
welingtoncassis <welingtoncassis@gmail.com>
2021-06-25 11:46:55 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const routes = require("./routes");
+const routes = require('./routes/routes');
 
 let port = process.env.PORT || 3000;
 
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
 app.listen(port, () => {
   console.log(`The server is lintening on port ${port}`);
